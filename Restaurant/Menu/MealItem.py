b5d9678f3e1eedd039ece446dc51c8c9d9096ad1
Mvergara2 <MVERGAR4@depaul.edu>
2021-03-21 22:02:57 -0500

    upload

--- Restaurant/Menu/MealItem.py
+++ Restaurant/Menu/MealItem.py
@@ -1,7 +1,11 @@
 from Restaurant.Menu.Meal import Meal
+rom flask import Flask
+from flask import render_template, request, redirect, jsonify, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
 class MealItem:
     def __init__(self, ID:int, quantity: int):
-        self.ID = 1
+        self.ID = db.Column(db.Integer, primary_key=True)
         self.quantity = 0
 
     def updateQuantity(self, quantity: int):
